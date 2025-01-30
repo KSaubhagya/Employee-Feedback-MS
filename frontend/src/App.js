bfdb930b7ee2d7608c4513fdeeb3aed759598e9d
KSaubhagya <saubhagyakrhmk.21@uom.lk>
2025-01-30 20:17:10 +0530

    stable 4.1 (table, nav, dummy dt)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
+import Navbar from "./components/Navbar"; 
 
 function App() {
   return (
     <Router>
+      <Navbar /> {/* Add the Navbar component here */}
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/feedback" element={<FeedbackForm />} />
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/Navbar.css";
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    Employee Feedback System
+                </Link>
+                <ul className="navbar-menu">
+                    <li className="navbar-item">
+                        <Link to="/" className="navbar-links">Home</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link to="/feedback" className="navbar-links">Feedback</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link to="/feedback-list" className="navbar-links">Feedback List</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
